64315a097fc3941f7948c1fadaaab07df3bc7032
kmftang <kmftangchaoyang@gmail.com>
2024-12-20 15:16:30 +0800

    now logger can change log file name

--- collective/logger.py
+++ collective/logger.py
@@ -21,5 +21,26 @@ def _setup_logger(name='collective_logger', log_file='logs/collective_time.json'
     
     return logger
 
+def set_log_file(log_file: str):
+    """Change the output log file
+    
+    Args:
+        log_file: New path for the log file
+    """
+    global logger
+    
+    # Remove existing handlers
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+    
+    # Create logs directory if it doesn't exist
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    
+    # Add new handler with the new log file
+    handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 # Initialize logger when module is imported
 logger = _setup_logger() 
